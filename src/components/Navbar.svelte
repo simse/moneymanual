e12167c70a90beb460eee9d43a5cffe4c2303e05
Simon Sorensen <hello@simse.io>
2026-05-10 12:39:42 +0100

    create a github actions workflow that runs on merges into the main branch, it's for deploying the website. first it runs code quality checks in parrallel, then deploys to staging, then runs the playwright e2e tests against the staging deployment, then deploys to prod

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -15,50 +15,60 @@ let isMenuOpen = $state(false);
 </script>
 
 <nav class="w-full bg-black text-white">
-  <ul class="flex items-center gap-1 mx-auto w-full max-w-5xl pl-4">
-    <li class="mr-auto">
-      <a class="font-bold text-xl hover:border-b-2 active:bg-yellow-400 active:text-black" href="/">
-        moneymanual<span class="opacity-70">.org.uk</span>
-      </a>
-    </li>
+    <ul class="flex items-center gap-1 mx-auto w-full max-w-5xl pl-4">
+        <li class="mr-auto">
+            <a
+                class="font-bold text-xl hover:border-b-2 active:bg-yellow-400 active:text-black"
+                href="/"
+            >
+                moneymanual<span class="opacity-70">.org.uk</span>
+            </a>
+        </li>
 
-    <li>
-      <button
-        class={{
-          "font-bold border-b-2 border-transparent hover:border-white uppercase flex items-center gap-1 p-4 hover:cursor-pointer": true,
-          "bg-white text-emerald-800": isMenuOpen,
-        }}
-        onclick={() => isMenuOpen = !isMenuOpen}
-      >
-        Topics <ChevronDownIcon />
-      </button>
-    <li>
-    <li class="border-l border-zinc-400 flex items-center">
-      <a class="pl-3 h-full hover:cursor-pointer" href="/search">
-        <SearchIcon class="h-6 w-6" />
-      </a>
-    </li>
-  </ul>
+        <li>
+            <button
+                class={{
+                    "font-bold border-b-2 border-transparent hover:border-white uppercase flex items-center gap-1 p-4 hover:cursor-pointer": true,
+                    "bg-white text-emerald-800": isMenuOpen,
+                }}
+                onclick={() => (isMenuOpen = !isMenuOpen)}
+            >
+                Topics <ChevronDownIcon />
+            </button>
+        </li>
+        <li></li>
+        <li class="border-l border-zinc-400 flex items-center">
+            <a class="pl-3 h-full hover:cursor-pointer" href="/search">
+                <SearchIcon class="h-6 w-6" />
+            </a>
+        </li>
+    </ul>
 </nav>
 
-<div class={{
-     "w-full bg-white": true,
-     "block": isMenuOpen,
-     "hidden": !isMenuOpen,
-}}>
-  <div class="px-4 py-8 mx-auto max-w-5xl">
-    <ul class="grid gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-       {#each topics as topic}
-      <li>
-        <a
-          href={topic.href}
-          class="group"
-        >
-          <span class="text-teal-700 group-hover:text-teal-900 font-bold text-xl underline">{topic.title}</span>
-          <p class="text-zinc-700 group-hover:text-zinc-900 font-medium">{topic.description}</p>
-        </a>
-      </li>
-       {/each}
-    </ul>
-  </div>
+<div
+    class={{
+        "w-full bg-white": true,
+        block: isMenuOpen,
+        hidden: !isMenuOpen,
+    }}
+>
+    <div class="px-4 py-8 mx-auto max-w-5xl">
+        <ul class="grid gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+            {#each topics as topic}
+                <li>
+                    <a href={topic.href} class="group">
+                        <span
+                            class="text-teal-700 group-hover:text-teal-900 font-bold text-xl underline"
+                            >{topic.title}</span
+                        >
+                        <p
+                            class="text-zinc-700 group-hover:text-zinc-900 font-medium"
+                        >
+                            {topic.description}
+                        </p>
+                    </a>
+                </li>
+            {/each}
+        </ul>
+    </div>
 </div>
